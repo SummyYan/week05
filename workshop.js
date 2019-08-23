21da4c1693f6450dce4e10228d3cd85b4e023df1
SummyYan <summy.y.yan@gmail.com>
2019-08-23 18:57:40 +1000

    fixed localhost problem

---
+++ workshop.js
@@ -0,0 +1,17 @@
+// let express = require('express');
+// let app = express();
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+// app.use(express.static('public'));
+// let date = new Date();
+// app.get('/getdate',function(req,res){
+//     res.send('date '+ date.getDate());
+ 
+// });
+
+// app.get('/gettime',function(req,res){
+//     res.send('time '+ date.getHours()+':'+date.getMinutes());
+ 
+// });
+
+// app.listen(8080);
