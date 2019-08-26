0c6c6707f351da134b4ef344b7cde6c9122ec95c
SummyYan <summy.y.yan@gmail.com>
2019-08-26 14:55:25 +1000

    update3

--- app.js
+++ app.js
@@ -32,13 +32,14 @@ app.get('/',function(req,res){
 app.get('/newtask',function(req,res){
     res.sendFile(__dirname+'/newtask.html');
 });
-app.post('/newtask',function(req,res){
+app.post('/addnewtask',function(req,res){
     
     // taskDb.push({
     //     taskName: req.body.taskName,
     //     taskDue: req.body.taskDue,
     //     taskDesc: req.body.taskDesc
     // });
+    
     taskDb.push(req.body);
     console.log(req.body);
     console.log('I have '+ taskDb.length+' record');
@@ -49,6 +50,6 @@ app.get('/listtasks',function(req,res){
     res.render('listtasks.html',{taskDb: taskDb});
 });
 app.listen(8000);
-console.log('server running at http://127.0.0.1:8000/');
+console.log('server running at http://localhost:8000/');
 
 //dynamic files: get the data at run time -- using view engine
